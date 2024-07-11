79d95c7698bf22de252c4b060a03abca2d6c39ee
vanparfait <nguemechiaparfait@gmail.com>
2024-07-11 11:20:54 +0100

    realisation de la partie de droite

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         myBlue: "#DAE6F3",
       },
+      backgroundColor: {
+        "custom-cyan": "#22d3ee", // Ajout d'une couleur de fond personnalisée
+      },
     },
   },
   plugins: [],
